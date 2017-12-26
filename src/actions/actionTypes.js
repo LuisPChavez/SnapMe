0580c5554560a94f58b0a4ff32397da929e98ec5
LuisPChavez <luis.p.chavez.1@gmail.com>
2017-12-26 11:41:55 -0800

    Made Options Scene

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -2,10 +2,4 @@ const albumChanged = "ALBUM_CHANGED"
 const ToAlbumsScene = "albumsScene"
 const ToGalleryScene = "galleryScene"
 
-
-
-
-
-
-
 export {albumChanged,ToAlbumsScene,ToGalleryScene};
--- src/components/OptionsScene.js
+++ src/components/OptionsScene.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
 import Header from './Header'
 import ButtonWithSwitch from './ButtonWithSwitch'
 
@@ -16,4 +18,23 @@ class OptionsScene extends Component {
     }
 }
 
-export default OptionsScene;
+function mapStateToProps(state) {
+    return {
+        //All Options here
+    };
+}
+
+function matchDispatchToProps(dispatch){
+    return bindActionCreators({/* Options here,*/togalleryscene}, dispatch);
+    //const act = bindActionCreators({ChangeAlbum: ChangeAlbum}, dispatch)
+    //console.log(bindActionCreators({ChangeAlbum: ChangeAlbum}, dispatch))
+    
+    /*console.log(act)
+    return {
+        togalleryscene,
+        act
+    };*/
+
+
+}
+export default connect(mapStateToProps,matchDispatchToProps)(OptionsScene);
